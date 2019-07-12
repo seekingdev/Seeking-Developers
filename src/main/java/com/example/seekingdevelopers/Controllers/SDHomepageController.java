0168fbc306618b3e454e9f70942dceea7c02536e
dereknoblej <derek.noblej@gmail.com>
2019-07-12 16:00:55 -0500

    home page is now visible

--- src/main/java/com/example/seekingdevelopers/Controllers/SDHomepageController.java
+++ src/main/java/com/example/seekingdevelopers/Controllers/SDHomepageController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class
- SDHomepageController {
+public class SDHomepageController {
+
     @GetMapping("/home")
     public String returnHomePage() {
         return "home";
