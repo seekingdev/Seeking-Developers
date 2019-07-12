82aa44b6daed2372f20f3fd88de21284852490ed
ElleBerry <48695394+ElleBerry@users.noreply.github.com>
2019-07-12 15:45:54 -0500

    changed landing page path

--- src/main/java/com/example/seekingdevelopers/Controllers/SDHomepageController.java
+++ src/main/java/com/example/seekingdevelopers/Controllers/SDHomepageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class
  SDHomepageController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String returnHomePage() {
         return "home";
     }
