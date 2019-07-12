fa2089cc972eaa2beb71439b23f609332def7fed
dereknoblej <derek.noblej@gmail.com>
2019-07-12 10:19:00 -0500

    initial commit

---
+++ src/main/java/com/example/seekingdevelopers/SeekingdevelopersApplication.java
@@ -0,0 +1,13 @@
+package com.example.seekingdevelopers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeekingdevelopersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeekingdevelopersApplication.class, args);
+    }
+
+}
