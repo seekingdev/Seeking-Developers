176fd0caf80086ccc7501ed49489a66ebc0359cc
Nic Tipton <ngtipton@gmail.com>
2019-07-12 13:55:02 -0500

    Updating

--- src/main/java/com/example/seekingdevelopers/Repositories/ProjectRepository.java
+++ src/main/java/com/example/seekingdevelopers/Repositories/ProjectRepository.java
@@ -1,11 +1,10 @@
 package com.example.seekingdevelopers.Repositories;
 
-import com.example.seekingdevelopers.Repositories.;
+import com.example.seekingdevelopers.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+
 }
--- src/main/java/com/example/seekingdevelopers/Repositories/UserRepository.java
+++ src/main/java/com/example/seekingdevelopers/Repositories/UserRepository.java
@@ -1,11 +1,10 @@
 package com.example.seekingdevelopers.Repositories;
 
-import com.example.seekingdevelopers.Repositories.;
+import com.example.seekingdevelopers.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    User findByUsername(String username);
-    User findByEmail(String email);
+
 }
