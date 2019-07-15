1381773a87ab4d890632b81f20745c347a8598b5
ElleBerry <48695394+ElleBerry@users.noreply.github.com>
2019-07-15 11:43:02 -0500

    dashboard view

--- src/main/java/com/example/seekingdevelopers/Controllers/DashboardController.java
+++ src/main/java/com/example/seekingdevelopers/Controllers/DashboardController.java
@@ -1,9 +1,14 @@
 package com.example.seekingdevelopers.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dahsboard";
+    }
 
 }
