31392dea1b8ecaead5ad75977243ee64330fc35e
dereknoblej <derek.noblej@gmail.com>
2019-07-17 14:12:10 -0500

    profile controller bascics complete

--- src/main/java/com/example/seekingdevelopers/Controllers/ProfileController.java
+++ src/main/java/com/example/seekingdevelopers/Controllers/ProfileController.java
@@ -2,11 +2,15 @@ package com.example.seekingdevelopers.Controllers;
 
 import com.example.seekingdevelopers.Repositories.ProjectRepository;
 import com.example.seekingdevelopers.Repositories.UserRepository;
+import com.example.seekingdevelopers.models.Project;
 import com.example.seekingdevelopers.models.User;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ProfileController {
@@ -22,6 +26,11 @@ public class ProfileController {
     public String loggedInProfile(Model model){
         User loggedinUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("user",loggedinUser);
+        Project lastProject = projectDao.findDistinctTopByCreatorOrderByCreatingDateDesc(loggedinUser);
+        model.addAttribute("lastProject",lastProject);
+       ArrayList<Project> projects = projectDao.findAllByCreatorOrderByCreatingDate(loggedinUser);
+        model.addAttribute("projects", projects);
+
         return "users/profile";
     }
 
--- src/main/java/com/example/seekingdevelopers/Repositories/ProjectRepository.java
+++ src/main/java/com/example/seekingdevelopers/Repositories/ProjectRepository.java
@@ -1,10 +1,18 @@
 package com.example.seekingdevelopers.Repositories;
 
 import com.example.seekingdevelopers.models.Project;
+import com.example.seekingdevelopers.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    Project findDistinctTopByCreatorOrderByCreatingDateDesc(User user);
+
+     ArrayList<Project> findAllByCreatorOrderByCreatingDate(User user);
 
 }
