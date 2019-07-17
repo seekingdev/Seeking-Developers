c5a4f4861007128a5357de5c5438227ea0fe2b2d
dereknoblej <derek.noblej@gmail.com>
2019-07-17 15:02:52 -0500

    dashboard is populating with projects

--- src/main/java/com/example/seekingdevelopers/Controllers/DashboardController.java
+++ src/main/java/com/example/seekingdevelopers/Controllers/DashboardController.java
@@ -1,14 +1,29 @@
 package com.example.seekingdevelopers.Controllers;
 
+import com.example.seekingdevelopers.Repositories.ProjectRepository;
+import com.example.seekingdevelopers.Repositories.UserRepository;
+import com.example.seekingdevelopers.models.Project;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class DashboardController {
 
-    @GetMapping("/dashboard")
-    public String dashboard(){
+    private final ProjectRepository projectDao;
+    private final UserRepository userDao;
 
+    public DashboardController(ProjectRepository projectDao, UserRepository userDao){
+        this.projectDao = projectDao;
+        this.userDao = userDao;
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(Model model){
+        ArrayList<Project> listOfProjects = projectDao.findAllByisCompleteFalseOrderByCreatingDateDesc();
+        model.addAttribute("listOfProjects", listOfProjects);
         return "projects/dashboard";
     }
 
