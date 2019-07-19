b3d4200f59f3a99db3314249261e423b11014731
dereknoblej <derek.noblej@gmail.com>
2019-07-19 11:26:51 -0500

    List user page and single user page is created

--- src/main/java/com/example/seekingdevelopers/Repositories/UserRepository.java
+++ src/main/java/com/example/seekingdevelopers/Repositories/UserRepository.java
@@ -4,8 +4,12 @@ import com.example.seekingdevelopers.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+
+    List<User> findAll();
 
 }
