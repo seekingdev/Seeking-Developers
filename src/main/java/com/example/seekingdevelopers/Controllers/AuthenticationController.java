58f6621dd36c8ddeaf7eed9b9bfd799481052121
dereknoblej <derek.noblej@gmail.com>
2019-10-08 11:05:22 -0500

    signup page has been created using foundation

--- src/main/java/com/example/seekingdevelopers/Controllers/AuthenticationController.java
+++ src/main/java/com/example/seekingdevelopers/Controllers/AuthenticationController.java
@@ -42,7 +42,7 @@ public class AuthenticationController {
     @GetMapping("/signup")
     public String showSignupForm(Model model){
         model.addAttribute("user", new User());
-        return "users/signup";
+        return "users/signup-foundation";
     }
     @PostMapping("/signup")
     public String saveUser(@Valid User user, Errors validation, Model model, @RequestParam(name = "dev_type") Long id, @RequestParam(name = "language")long[] languages){
